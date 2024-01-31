f682c7ba5cc8b8de0834aa16aa10e3d6c6e74444
vyastreb <profywld@gmail.com>
2024-01-31 10:40:10 +0100

    Improved code, added config file and corrected the bug of inappropriate parameter reading.

--- MistralTerminal.py
+++ MistralTerminal.py
@@ -4,40 +4,64 @@ This script is a terminal interface for MistralAI.
 It is a simple script that allows you to ask questions to MistralAI directly in terminal and get (relatively) short answers.
 V.A. Yastrebov, CNRS, MINES Paris - PSL, France, Jan 2024.
 License: BSD 3 clause
+
+Handle arguments:
+--help/-h:                writes help
+--temp/-T:                sets temperature
+--tokens/-t:              sets number of tokens
+--model/-m:               sets the model
+--verbose/-v:             prints the question
+--not-chat/-n:            switchs off chat mode, does not keep previous answers
+       :                  text of the question - after ":" the question goes
+
+It stores the history of previous answers in a file in home directory `.mistralai/history.txt`
+The configuration file `.mistralai/config.json` is used to store the parameters.
 """
 
 from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
-import os, sys, re, time
+import os, sys, re, time, json
 from collections import deque
 
-# Handle arguments
-# --help/-h:                writes help
-# --temp/-T:                sets temperature
-# --tokens/-t:              sets number of tokens
-# --model/-m:               sets the model
-# --verbose/-v:             prints the question
-# --not-chat/-n:            switchs off chat mode, does not keep previous answers
-# : Text of the question:   after ":" the question goes
+########################################
+#     Global parameters
+########################################
 
-# Default values
+CONFIG_PATH  = os.path.expanduser("~/.mistralai/config.json")
+HISTORY_PATH = os.path.expanduser("~/.mistralai/history.txt")
+
 # ANSI color codes
 RED = '\033[91m'
 GREEN = '\033[92m'
 YELLOW = '\033[93m'
 BLUE = '\033[94m'
+# Answers (code blocks) will be printed in ANSWER_COLOR (CODE_COLOR), choose the one which is visible on your terminal in both light and dark mode
 ANSWER_COLOR = BLUE
 CODE_COLOR = RED
+# Activate emoji support
 EMOJI = True
+# Default MistralAI parameters: temperature T0, maximal number of used tokens TokenMax, and model
+T0          = 0.3
+TokenMax    = 100
+model       = "mistral-tiny"
 
-model = "mistral-tiny"
-max_depth = 31 # Maximum number of previous answers to keep in memory in chat mode, should be odd
-T0 = 0.2
-TokenMax = 1
-waitingTime = 180 # Time in seconds after which the history is erased
-PrintQuestion = False
-max_length = 80
-Chat = True
+########################################
+#     MistralAI client
+########################################
+
+# Retrieve API key
+try:
+    api_key = os.environ["MISTRAL_API_KEY"]
+except KeyError:
+    print("Error: MISTRAL_API_KEY environment variable is not set.")
+    exit()
+
+# create MistralAI client
+client = MistralClient(api_key=api_key)
+
+########################################
+#     Functions
+########################################
 
 def Role(i, size):
         if i % 2 == 0:
@@ -45,18 +69,20 @@ def Role(i, size):
         else:
             return "assistant"
 
-def follow_chat(previous_answers : deque,temperature=T0, max_tokens=TokenMax):
-    if len(previous_answers) == 1:
-        return answer_question(previous_answers[0],temperature=temperature, max_tokens=max_tokens)
+def follow_chat(chat_history : deque, temperature=T0, max_tokens=TokenMax):
+    if len(chat_history) == 1:
+        return answer_question(chat_history[0],temperature=temperature, max_tokens=max_tokens)
     else:
         messages = []
-        for i in range(len(previous_answers)):
-            messages.append(ChatMessage(role=Role(i,len(previous_answers)), content=previous_answers[i],temperature=temperature, max_tokens=max_tokens))
+        for i in range(len(chat_history)):
+            messages.append(ChatMessage(role=Role(i,len(chat_history)), content=chat_history[i],temperature=temperature, max_tokens=max_tokens))
         # print("\n\n == MESSAGES == \n\n", messages )
         try:
             chat_response = client.chat(
                 model=model,
                 messages=messages,
+                temperature=temperature,
+                max_tokens=max_tokens,
             )
             bot_response = chat_response.choices[0].message.content
         except Exception as e:
@@ -71,6 +97,8 @@ def answer_question(my_question,temperature=T0, max_tokens=TokenMax):
         chat_response = client.chat(
             model=model,
             messages=messages,
+            temperature=temperature, 
+            max_tokens=max_tokens,
         )
         bot_response = chat_response.choices[0].message.content
         return bot_response
@@ -85,22 +113,22 @@ def print_in_box(text, color_code):
     RESET = ANSWER_COLOR
     lines = text.split('\n')
     # Calculate max length considering the visible characters only
-    max_length = max(len(strip_ansi_codes(line)) for line in lines)
+    max_line_length = max(len(strip_ansi_codes(line)) for line in lines)
 
     if EMOJI:
         header = " < 🤖 > "
-        upper_border = '+' + '--' + header + "-" * (max_length - len(header) - 3) + '--' + '+'
+        upper_border = '+' + '--' + header + "-" * (max_line_length - len(header) - 3) + '--' + '+'
     else:
         header = " AI REPLY "
-        upper_border = '+' + '--' + header + "-" * (max_length - len(header) - 2) + '--' + '+'
-    lower_border = '+' + '-' * (max_length + 2) + '+'
+        upper_border = '+' + '--' + header + "-" * (max_line_length - len(header) - 2) + '--' + '+'
+    lower_border = '+' + '-' * (max_line_length + 2) + '+'
 
     bold_pattern = re.compile(r'\*\*(.*?)\*\*')
     italic_pattern = re.compile(r'\*(.*?)\*')
 
     print(color_code + upper_border)
     for line in lines:
-        print('| ' + line + ' ' * (max_length - len(strip_ansi_codes(line))) + ' |')
+        print('| ' + line + ' ' * (max_line_length - len(strip_ansi_codes(line))) + ' |')
     print(lower_border + '\033[0m')  # Reset to default color at the end
 
 def split_long_lines(input_string, max_line_length):
@@ -163,80 +191,131 @@ def colorize_text(text):
 
     return colored_text
 
-# Parse arguments
-for i in range(len(sys.argv)):
-    if sys.argv[i] == "--help" or sys.argv[i] == "-h":
-        print("Usage: python3 MistralTerminal.py\n \
-            --model/-m:     sets the model\n \
-            --temp/-T:      sets temperature\n \
-            --tokens/-t:    sets number of tokens\n \
-            --not-chat/-n:  switchs off chat mode, does not keep previous answers\n \
-            --help/-h:      writes help")
-        print("Example: python3 MistralTerminal.py --model mistral-tiny --temp 0.2 --tokens 5")
-        print("Default values:\n   model=mistral-tiny\n   temperature=0.2\n   number_of_tokens=2")
-        exit()
-    elif sys.argv[i] == "--model" or sys.argv[i] == "-m":
-        model = sys.argv[i+1]
-    elif sys.argv[i] == "--verbose" or sys.argv[i] == "-v":
-        PrintQuestion = True
-    elif sys.argv[i] == "--temp" or sys.argv[i] == "-T":
-        T0 = float(sys.argv[i+1])
-    elif sys.argv[i] == "--tokens" or sys.argv[i] == "-t":
-        TokenMax = int(sys.argv[i+1])        
-    elif sys.argv[i] == "--not-chat" or sys.argv[i] == "-n":
-        Chat = False
-    else:
-        if i == 0:
-            continue
-        if sys.argv[i] == ":":
-            # Concatenate everything after ":" into a single string.
-            my_question = " ".join(sys.argv[i+1:])
-            break
+########################################
+#     Main
+########################################
 
-# Previous answers are kept in ~.mistralai/history.txt, check that it exists, and if it exists and not older than 1 minute, read it, otherwise erase it and create a new one
+def main():    
+    global T0, TokenMax, model
+    # Local parameters: chat and output parameters
+    Chat = True
+    verbose = False
+    max_memory = 31 # Maximum number of previous answers to keep in memory in chat mode, should be odd
+    waitingTime = 180 # Time in seconds after which the history is erased
+    max_line_length = 80      
 
-if Chat:
-    history_file_path = os.path.expanduser("~/.mistralai/history.txt")
+    time0 = time.time()
+    # Checks whether config file exists: if yes, read parameters from there, otherwise use default values
+    ArgumentsProvenance = "Default values"
+    if os.path.isfile(CONFIG_PATH):
+        try:
+            with open(CONFIG_PATH, 'r') as file:
+                ArgumentsProvenance = "Configuration file"
+                config = json.load(file)
+                model = config["model"]
+                max_memory = config["max_memory"]
+                T0 = config["temperature"]
+                TokenMax = config["max_tokens"]
+                waitingTime = config["waitingTime"]
+                max_line_length = config["max_line_length"]
+        except json.JSONDecodeError as e:
+            print(f"Error reading JSON file: {e}")
+        except KeyError as e:
+            print(f"Missing key in JSON file: {e}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
-    try:
-        if os.path.isfile(history_file_path):
-            if os.path.getmtime(history_file_path) > time.time() - waitingTime:
-                with open(history_file_path, 'r') as f:
-                    previous_answers = deque(f.read().split('$$##'), maxlen=max_depth)
-                with open(history_file_path, 'a') as f:
-                    f.write('$$##' + my_question)
-                previous_answers.append(my_question)
-            else:
-                previous_answers = deque([my_question], maxlen=max_depth)
-                with open(history_file_path, 'w') as f:
-                    f.write(my_question)
+
+    # Parse arguments
+    for i in range(len(sys.argv)):
+        if sys.argv[i] == "--help" or sys.argv[i] == "-h":
+            print("Usage: python3 MistralTerminal.py\n \
+                --model/-m:     sets the model\n \
+                --temp/-T:      sets temperature\n \
+                --tokens/-t:    sets number of tokens\n \
+                --not-chat/-n:  switchs off chat mode, does not keep previous answers\n \
+                --help/-h:      writes help")
+            print("Example: python3 MistralTerminal.py --model mistral-tiny --temp 0.2 --tokens 50")
+            print("Default values:\n   model={}\n   temperature={}\n   number_of_tokens={}".format(model,T0,TokenMax))
+            exit()
+        elif sys.argv[i] == "--model" or sys.argv[i] == "-m":
+            ArgumentsProvenance = "Command line"
+            model = sys.argv[i+1]
+        elif sys.argv[i] == "--verbose" or sys.argv[i] == "-v":
+            verbose = True
+        elif sys.argv[i] == "--temp" or sys.argv[i] == "-T":
+            ArgumentsProvenance = "Command line"
+            T0 = float(sys.argv[i+1])
+        elif sys.argv[i] == "--tokens" or sys.argv[i] == "-t":
+            ArgumentsProvenance = "Command line"
+            TokenMax = int(sys.argv[i+1])        
+        elif sys.argv[i] == "--not-chat" or sys.argv[i] == "-n":
+            Chat = False
         else:
-            previous_answers = deque([my_question], maxlen=max_depth)
-            with open(history_file_path, 'w') as f:
-                f.write(my_question)
-    except Exception as e:
-        print("An error occurred:", e)
+            if i == 0:
+                continue
+            if sys.argv[i] == ":":
+                # Concatenate everything after ":" into a single string.
+                my_question = " ".join(sys.argv[i+1:])
+                break
 
+    if verbose:
+        if ArgumentsProvenance == "Default values":
+            print("No configuration file found, using default parameters:")
+        elif ArgumentsProvenance == "Command line":
+            print("Using parameters from command line:")
+        elif ArgumentsProvenance == "Configuration file":            
+            print("Configuration file found, using parameters from there:")
+        else:
+            print("Warning: unknown provenance of parameters, using default values.")
+        print(" \nmodel: {} \
+                \nmax_memory: {} \
+                \ntemperature: {} \
+                \nmax_tokens: {} \
+                \nwaitingTime: {} \
+                \nmax_line_length: {}".format(model,max_memory,T0,TokenMax,waitingTime,max_line_length))
 
-if PrintQuestion:
-    print("<"+my_question+">")
+    # Previous answers are kept in HISTORY_PATH, check that it exists, and if it exists and not older than 1 minute, read it, otherwise erase it and create a new one
     if Chat:
-        print("Previous answers:",previous_answers)
+        try:
+            if os.path.isfile(HISTORY_PATH):
+                if os.path.getmtime(HISTORY_PATH) > time.time() - waitingTime:
+                    with open(HISTORY_PATH, 'r') as f:
+                        chat_history = deque(f.read().split('$$##'), maxlen=max_memory)
+                    with open(HISTORY_PATH, 'a') as f:
+                        f.write('$$##' + my_question)
+                    chat_history.append(my_question)
+                else:
+                    chat_history = deque([my_question], maxlen=max_memory)
+                    with open(HISTORY_PATH, 'w') as f:
+                        f.write(my_question)
+            else:
+                chat_history = deque([my_question], maxlen=max_memory)
+                with open(HISTORY_PATH, 'w') as f:
+                    f.write(my_question)
+        except Exception as e:
+            print("An error occurred:", e)
 
 
-# Retrieve API key
-api_key = os.environ["MISTRAL_API_KEY"]
+    if verbose:
+        print("Question: <"+my_question+">")
+        if Chat:
+            for i in range(len(chat_history)):
+                message = chat_history[i].replace('\n\n',' ').replace('\n',' ').replace('\r',' ').replace('\t',' ')
+                print("Previous messages #{}: <{}>".format(i,message))
 
-# Request to MistralAI
-client = MistralClient(api_key=api_key)
-
-if Chat:
-    answer = follow_chat(previous_answers,temperature=T0, max_tokens=TokenMax)
-else:
-    answer = answer_question(my_question,temperature=T0, max_tokens=TokenMax)   
-if Chat:
-    previous_answers.append(answer)
-    with open(os.path.expanduser("~/.mistralai/history.txt"), 'w') as f:
-        f.write('\n$$##\n'.join(previous_answers))
-adjusted_text = split_long_lines_preserving_breaks(answer,max_length)
-print_in_box(colorize_text(adjusted_text), ANSWER_COLOR)
+    print("Time it takes to formulate the question and grab the history: {:.2f} s".format(time.time()-time0))
+    time0 = time.time()
+    if Chat:
+        answer = follow_chat(chat_history,temperature=T0, max_tokens=TokenMax)
+    else:
+        answer = answer_question(my_question,temperature=T0, max_tokens=TokenMax)   
+    if Chat:
+        chat_history.append(answer)
+        with open(os.path.expanduser("~/.mistralai/history.txt"), 'w') as f:
+            f.write('\n$$##\n'.join(chat_history))
+    adjusted_text = split_long_lines_preserving_breaks(answer,max_line_length)
+    print_in_box(colorize_text(adjusted_text), ANSWER_COLOR)
+    print("Time it takes to get the answer: {:.2f} s".format(time.time()-time0))
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # MistralTerminal - chatbot in your Linux terminal
 
 ## Overview
+
 MistralTerminal is a command-line interface for interacting with MistralAI. It allows users to send questions directly from the terminal and receive concise answers from MistralAI. This script is designed for simplicity and ease of use. It keeps the conversation in a local file, so you can continue interact with it without giving the context every time. It also supports colored output for enhanced readability.
 
 ## Author
+
 V.A. Yastrebov, CNRS, MINES Paris - PSL, France, Jan 2024.
 License: BSD 3 clause
 
 ## Requirements
+
 - Python 3
 - mistralai Python package `pip install mistralai`
 - An API key for MistralAI, check on [mistral.ai](https://mistral.ai)
 
 ## Installation
+
 Ensure Python 3 and the necessary packages are installed. Set your MistralAI API key in your environment:
 
 ```bash
@@ -26,6 +30,7 @@ alias ai="python3 /path/to/script/MistralTerminal.py 2>/dev/null "
 
 
 ### Options
+
 - `--model/-m`: Sets the MistralAI model to be used. Default is 'mistral-tiny'.
 - `--temp/-T`: Sets the temperature for the AI's responses. Default is 0.2.
 - `--tokens/-t`: Sets the maximum number of tokens in the response. Default is 2.
@@ -35,7 +40,27 @@ alias ai="python3 /path/to/script/MistralTerminal.py 2>/dev/null "
 
 The default model is 'mistral-tiny', which is a smaller model that is faster to load and run. The default temperature is 0.2, which is a good value for most questions. The default token count is 2, which is a good value for most questions. The verbose option is useful for checking whether your question was correctly parsed by the script. For example, if you have `#` or `"` in your question, you need to put it with `\#`, `\"`, the same with brackets.
 
-## Usage
+### Configuration file
+
+All parameters could be defined in the script or in the config file `~/.mistralai/config.json`. 
+If config file is not found, the script will use default parameters.
+If config file exists, it will use parameters from the file, and will ignore parameters defined in the script.
+But if you prescribe options in the command line, they will be used instead of the config file.
+
+*Example of config file:*
+```
+{
+ "model": "mistral-tiny",
+ "max_memory": 31,
+ "max_tokens": 1000,
+ "waitingTime": 180,
+ "max_line_length": 80,
+ "temperature": 0.5
+}
+``` 
+
+### Usage
+
 The script can be run from the command line with various options. Enter your question preceded by `:` and the script will process and display the AI's response.
 
 ```bash
@@ -56,16 +81,16 @@ ai --temp 0.5 : What is the meaning of life?
 ai --model mistral-tiny --temp 0.2 --tokens 5 : What is best cheese in France?
 ```
 
-## Features
+### Features
 
 - Can be run from anywhere in the terminal
 - Keeps the history of conversation in a local file for some time
 - Colored output for enhanced readability
 - Adjustable parameters for model, temperature, and token count
-- Supports multiline responses with automatic line wrapping
+- Supports multi-line responses with automatic line wrapping
 
-## Notes
+### Notes
 
 - Ensure your terminal supports ANSI color codes for the best experience.
-- The history of last messages (30 by default) is stored in `~/.mistralai/history.txt` for 3 minutes by default. You can change the number of messages and the time in the script: `max_depth` and `waitingTime`.
+- The history of last messages (31 by default) is stored in `~/.mistralai/history.txt` for 3 minutes by default. You can change the number of stored messages and the time in the script of in config file: `max_memory` and `waitingTime`.
 
